467fc86b574e15a1b9cce1dd80334836d34c4834
axelriv62 <axel_riviere@ens.univ-artois.fr>
2024-12-30 15:35:28 +0100

    :sparkles: Développement de l'accueil

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log('Happy developing ✨')
+document.querySelector('.burger-menu').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('active');
+});
